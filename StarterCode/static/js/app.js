f82b03e9cbbeae72e0efeb6192989139a836d535
Karen Gutierrez <kareng018@berkeley.edu>
2021-08-09 18:43:59 -0700

    final

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,6 +2,7 @@
 function init(){
     var dropdown = d3.select("#selDataset");
     d3.json("./samples.json").then((data) => {
+        // console.log(data)
         data.names.forEach((name)=> {
             dropdown.append("option").text(name).property("value");
         });

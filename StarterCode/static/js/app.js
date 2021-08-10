dff89708e2e2f9932307d589087eef1564161aed
Karen Gutierrez <kareng018@berkeley.edu>
2021-08-09 17:59:47 -0700

    demographic function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -19,9 +19,16 @@ function optionChanged(id){
 //function to generate the demographic table
 function demInfo(id){
     d3.json("samples.json").then((data) => {
-        
-    })
-}
+        var meta = data.metadata;
+        var result = meta.filter( meta => meta.id == id)[0];
+        var demoInfo = d3.select("#sample-metadata");
+
+        demoInfo.html("");
+        Object.entries(result).forEach((key) =>{
+            demoInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+        });
+    });
+};
 
 //Call init to initiate page
 init();
